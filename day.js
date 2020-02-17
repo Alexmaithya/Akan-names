fa24638a786d9f4d9a2f95544c0511dc4450c569
Alexmaithya <alexmaithya2001@gmail.com>
2020-02-17 10:51:40 +0300

    modification

--- day.js
+++ day.js
@@ -3,27 +3,28 @@ var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
 var weekDay = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 
 //function calling
-function getName() {
-    var century = parseInt(document.getElementById("CENTURY").value);
-    var year = parseInt(document.getElementById("YEAR").value);
-    var month = parseInt(document.getElementById("MONTH").value);
-    var day = parseInt(document.getElementById("DAY").value);
-    var male = document.getElementById("MALE");
-    var female = document.getElementById("FEMALE");
+function getAkanNames() {
+    var year = parseInt(document.getElementById("yy").value);
+    var month = parseInt(document.getElementById("mm").value);
+    var day = parseInt(document.getElementById("dd").value);
+    var male = document.getElementById("male");
+    var female = document.getElementById("female");
 
-}
+
 //validation
 if (day <= 0 || day > 31)
     alert("invalid option please enter a valid date");
-else if (MONTH <= 0 || MONTH > 12 || MONTH == 2 && DAY > 29)
+else if (month <= 0 || month > 12 || month == 2 && day > 29)
     alert("invalid option please enter a valid date");
 
-    var day = new Date(CENTURY + "/" +YEAR + "/" + MONTH+ "/" +DAY );
+    var day = new Date(year + "/" + month + "/" + day);
     var birthDay=day.getDay();
+
+
     if(male.checked==true){
        alert("You were born on " +weekDay[birthDay]+ " and Your Akan name is " + maleNames[birthDay])
     }
     else if(female.checked==true){
        alert("You were born on " +weekDay[birthDay]+ " and Your Akan name is " + femaleNames[birthDay])
     }
-    
+}
